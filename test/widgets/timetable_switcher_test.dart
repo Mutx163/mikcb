193f4d335f9f3ee811f46c9d07e7bc607669ee55
User <user@example.com>
2026-03-25 22:55:19 +0800

    发布: cut 1.1.9

--- test/widgets/timetable_switcher_test.dart
+++ test/widgets/timetable_switcher_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,9 +8,24 @@ import 'package:university_timetable/screens/timetable_screen.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  const homeWidgetChannel =
+      MethodChannel('com.example.university_timetable/home_widget');
+  const analyticsChannel =
+      MethodChannel('com.example.university_timetable/umeng_analytics');
 
   setUp(() {
     SharedPreferences.setMockInitialValues({});
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(homeWidgetChannel, (call) async => null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(analyticsChannel, (call) async => null);
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(homeWidgetChannel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(analyticsChannel, null);
   });
 
   testWidgets('home screen keeps timetable management in overflow menu only',
