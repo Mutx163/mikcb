7441c841d3505eb5c184041b3ec39831eff70be9
User <user@example.com>
2026-07-31 02:06:34 +0800

    Revert "debug(page): 临时移除 header 测试卡片点击"
    This reverts commit c58baab333a747160b6c265f827ade66a0e5795e.

--- lib/ui/hyperos/hyperos_page.dart
+++ lib/ui/hyperos/hyperos_page.dart
@@ -611,6 +611,12 @@ class _HyperosBlurredPageState extends State<_HyperosBlurredPage> {
                   : widget.child,
             ),
           ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: header,
+          ),
         ],
       ),
     ),
